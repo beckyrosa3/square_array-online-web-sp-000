5bec47f8dc88d2b3566e5de31f544a708bd29f39
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-18 04:51:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  array = [1,2,3]
+  array.each do |number**2|
+  puts array
 end
+
+numbers = [1,2,3]
+ 
+square_array(numbers)
+# => [1,4,9])
+ 
+new_numbers = [9,10,16,25]
+ 
+square_array(new_numbers)
+# => [81,100,256,625]
